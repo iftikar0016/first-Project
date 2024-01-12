f7cc4e3da75165c07f9a5283d322a737ef66c0da
Iftikar Alam <iftikaralam0016@gmail.com>
2024-01-12 21:44:07 +0530

    add files

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css';
+import Navigation from './components/navigation';
+import HeroSection from "./components/Hero";
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App
